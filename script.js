0868d0f4f9fd01795e914b1a0f3f09795a2aac71
Akriti Kushwaha <akritikushwaha028@gmail.com>
2026-07-21 21:25:20 +0530

    updated

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
-
-console.log(hamburger);
-console.log(navLinks);
+const navItems = document.querySelectorAll("#nav-links a");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
 });
